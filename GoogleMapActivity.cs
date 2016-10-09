a5047f9ef1fc687e32f8cc08c2f7c57339356ff3
bharathkumar1217 <bharathkumar1217@gmail.com>
2016-10-08 18:27:38 -0700

    Added second comment

--- GoogleMapActivity.cs
+++ GoogleMapActivity.cs
@@ -6,6 +6,9 @@ using Android.Gms.Maps.Model;
 using System;
 
 // Learning how to use git
+
+
+    //Afjsklkds;lglfd,phl;k,fdh/;f
 namespace GoogleMapApp
 {
 	[Activity ( Label = "Google Map", MainLauncher = true , Icon = "@drawable/icon" )]			
